41df4eead3f3697fe89eedc3b0bc641c0a87e4cd
Mehdi <mehdi@mehdi-aberkane.fr>
2017-08-08 22:29:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Asaku
+ * Date: 08/08/2017
+ * Time: 21:48
+ */
+
+require_once ('vendor/autoload.php');
+
+echo "salut";
+
---
+++ src/Entity/Post.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Asaku
+ * Date: 08/08/2017
+ * Time: 22:07
+ */
+class Post
+{
+    /**
+     * @var Integer $id
+     */
+    private $id;
+
+    /**
+     * @var String $title
+     */
+    private $title;
+
+    /**
+     * @var String $content
+     */
+    private $content;
+
+    /**
+     * @var DateTime $dateCreated
+     */
+    private $dateCreated;
+
+    private $user;
+
+    /**
+     * Post constructor.
+     */
+    public function __construct()
+    {
+        $this->dateCreated = New DateTime();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return Post
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * @param DateTime $dateCreated
+     * @return Post
+     */
+    public function setDateCreated($dateCreated)
+    {
+        $this->dateCreated = $dateCreated;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     * @return Post
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     * @return Post
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     * @return Post
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+        return $this;
+    }
+}
