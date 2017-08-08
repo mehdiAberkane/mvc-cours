5fcdf8215e283fc65d538ba64de594c9aa46aec9
Mehdi <mehdi@mehdi-aberkane.fr>
2017-08-08 22:39:00 +0200

    add folder

---
+++ db/MySQL.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Asaku
+ * Date: 08/08/2017
+ * Time: 22:35
+ */
+class MySQL
+{
+
+}
---
+++ test/TestIndex.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Asaku
+ * Date: 08/08/2017
+ * Time: 22:38
+ */
+class TestIndex
+{
+
+}
